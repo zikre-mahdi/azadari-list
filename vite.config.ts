b01482fab072daad94c05e1c2dfd1579dc9a799f
Minhal <jenansoft.india@gmail.com>
2026-06-18 00:14:50 +0530

    Add base directory to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -33,4 +33,5 @@ export default defineConfig({
 
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
+  base: '/azadari-list/',
 })
